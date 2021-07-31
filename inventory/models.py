4851be15a589f4a03d531cf4d75e5dde70687eb5
flavius t <fturdean@sfu.ca>
2021-07-31 17:15:21 +0300

    added temp versions of models

--- inventory/models.py
+++ inventory/models.py
@@ -6,3 +6,14 @@ class Ingredient(models.Model):
     name = models.CharField(primary_key=True, default="New Ingredient", max_length=30)
     cost = models.DecimalField(default=0.0, decimal_places=2)
     quantity = models.IntegerField(default=0)
+
+class MenuItem(models.Model):
+    name = models.CharField(primary_key=True, default="New Menu Item", max_length=30)
+    cost = models.DecimalField(default=0.0, decimal_places=2)
+
+class Recipe(models.Model):
+    name = models.CharField(primary_key=True, default="New Recipe", max_length=30)
+    instructions = models.CharField()
+
+class Order(models.Model):
+    
