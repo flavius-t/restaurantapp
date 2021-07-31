73f7e1145dea5ae16da2e7515b7343d599628e76
flavius t <fturdean@sfu.ca>
2021-07-31 16:44:28 +0300

    added Ingredient Model

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(primary_key=True, default="New Ingredient", max_length=30)
+    cost = models.DecimalField(default=0.0, decimal_places=2)
+    quantity = models.IntegerField(default=0)
