8de31be7335ae3c9bf7c0970a312c40b1696063a
flavius t <fturdean@sfu.ca>
2021-08-16 12:00:27 -0700

    finished first draft of models

--- inventory/models.py
+++ inventory/models.py
@@ -1,19 +1,23 @@
 from django.db import models
+from django.db.models.deletion import SET_NULL
 
 # Create your models here.
 
 class Ingredient(models.Model):
     name = models.CharField(primary_key=True, default="New Ingredient", max_length=30)
     cost = models.DecimalField(default=0.0, decimal_places=2)
-    quantity = models.IntegerField(default=0)
+    quantity = models.PositiveIntegerField(default=0)
+    unit = models.CharField(default="g", max_length=10)
 
 class MenuItem(models.Model):
     name = models.CharField(primary_key=True, default="New Menu Item", max_length=30)
     cost = models.DecimalField(default=0.0, decimal_places=2)
 
-class Recipe(models.Model):
-    name = models.CharField(primary_key=True, default="New Recipe", max_length=30)
-    instructions = models.CharField()
-
 class Order(models.Model):
-    
+    item = models.ForeignKey(MenuItem, on_delete=SET_NULL, blank=True, null=True)
+    time = models.DateTimeField()
+
+class RecipeRequirement(models.Model):
+   item = models.ForeignKey(MenuItem, on_delete=SET_NULL, blank=True, null=True)
+   ingredient = models.ForeignKey(Ingredient, on_delete=SET_NULL, blank=True, null=True) 
+   quantity = models.DecimalField(default=1)
