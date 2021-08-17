f27abb4731ac2bf7160dd80a79af215b9f5b9921
flavius t <fturdean@sfu.ca>
2021-08-17 01:44:08 -0700

    added IngredientList, fully functional

--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Ingredient
 
 # Create your views here.
 
 # Home page
 def home(request):
     return render(request, 'inventory/home.html')
+
+def IngredientList(request):
+    # SQL query to return list of all Ingredient tuples
+    ingredients = Ingredient.objects.raw("SELECT * FROM inventory_ingredient")
+    # 'data' is used in ingredientList.html to refer to 'ingredients'
+    context = {'data': ingredients}
+    return render(request, 'inventory/ingredientList.html', context)
