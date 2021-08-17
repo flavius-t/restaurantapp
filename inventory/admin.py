d8f5f00d2dcc0a332ec9c16a5b2be7546010640f
flavius t <fturdean@sfu.ca>
2021-08-16 23:15:46 -0700

    added admin and registered models

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Ingredient, MenuItem, Order, RecipeRequirement
+
 # Register your models here.
+admin.site.register(Ingredient)
+admin.site.register(MenuItem)
+admin.site.register(Order)
+admin.site.register(RecipeRequirement)
