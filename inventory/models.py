ac78b9f287f1ae21ba1770408d5e047c057cf59b
flavius t <fturdean@sfu.ca>
2021-08-18 13:39:26 -0700

    fixed DecimalField bug in models.py

--- inventory/models.py
+++ inventory/models.py
@@ -5,13 +5,13 @@ from django.db.models.deletion import SET_NULL
 
 class Ingredient(models.Model):
     name = models.CharField(primary_key=True, default="New Ingredient", max_length=30)
-    cost = models.DecimalField(default=0.0, decimal_places=2, max_digits=3)
+    cost = models.DecimalField(default=0.0, decimal_places=2, max_digits=5)
     quantity = models.PositiveIntegerField(default=0)
     unit = models.CharField(default="g", max_length=10)
 
 class MenuItem(models.Model):
     name = models.CharField(primary_key=True, default="New Menu Item", max_length=30)
-    cost = models.DecimalField(default=0.0, decimal_places=2, max_digits=3)
+    cost = models.DecimalField(default=0.0, decimal_places=2, max_digits=5)
 
 class Order(models.Model):
     item = models.ForeignKey(MenuItem, on_delete=SET_NULL, blank=True, null=True)
@@ -20,7 +20,7 @@ class Order(models.Model):
 class RecipeRequirement(models.Model):
    item = models.ForeignKey(MenuItem, on_delete=SET_NULL, blank=True, null=True)
    ingredient = models.ForeignKey(Ingredient, on_delete=SET_NULL, blank=True, null=True) 
-   quantity = models.DecimalField(default=1, decimal_places=2, max_digits=3)
+   quantity = models.DecimalField(default=1, decimal_places=2, max_digits=5)
 
    def __str__(self):
        return self.item + " " + self.ingredient + " " + self.quantity
