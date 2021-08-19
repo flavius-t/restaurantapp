5793e1563b8bb2e5c76336b08a2c11a39a6e65d2
flavius t <fturdean@sfu.ca>
2021-08-19 09:56:15 -0700

    added datetime selection widget to OrderCreate

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.forms.widgets import DateTimeInput
 from .models import Ingredient, Order, MenuItem, RecipeRequirement
+from django.contrib.admin.widgets import AdminSplitDateTime
 
 class IngredientCreateForm(forms.ModelForm):
     class Meta:
@@ -11,7 +13,14 @@ class MenuItemCreateForm(forms.ModelForm):
         model = MenuItem
         fields = ('name', 'cost')
 
+# date-time widget for use with OrderCreateForm
+class DateTimeInput(forms.DateTimeInput):
+    input_type = 'datetime-local'
+
 class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ('time', 'item_id')
+        fields = ('time', 'item')
+        widgets = {
+            'time': DateTimeInput(),
+        }
