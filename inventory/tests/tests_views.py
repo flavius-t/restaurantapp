55436414bacd52df60ae7fc50efe6e6a02b1b8dd
fturdean <fturdean@sfu.ca>
2022-04-12 12:29:08 -0700

    add order-create-view tests

--- inventory/tests/tests_views.py
+++ inventory/tests/tests_views.py
@@ -1,9 +1,14 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from inventory.models import Order, Ingredient, MenuItem
-import json
+from django.contrib.auth.models import User
 
-class TestViews(TestCase):
+from django.utils import timezone
+import datetime
+import pytz
+import zoneinfo
+
+class TestMenuItemListView(TestCase):
     def setUp(self):
        self.client = Client()
        self.list_url = reverse('menuitemlist')
@@ -12,20 +17,53 @@ class TestViews(TestCase):
     def test_ingredientlist_GET(self):
         response = self.client.get(self.list_url)
         self.assertEquals(response.status_code, 302)
-    
-    def test_ordercreate_POST_adds_new_order(self):
-        item = MenuItem.objects.create(
-            name='MenuItem1',
-            cost=20
-        )
-        context = {
-            'item': item,
-            'time': '2022-04-11 01:00:00'
-        }
-        response = self.client.post(self.order_create_url, context)
-        self.assertEquals(response.status_code, 302)
 
-        for order in Order.objects.all():
-            self.assertIsNotNone(order)
-            self.assertEquals(order.time, '2022-04-11 01:00:00')
-            self.assertEquals(order.item, item)
+class TestOrderCreateView(TestCase):
+    def setUp(self):
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        test_user1.save()
+
+        self.client = Client()
+        self.order_create_url = reverse('ordercreate')
+        self.menu_item_1 = MenuItem.objects.create(
+            name='menu-item-1',
+            cost=1.99
+        )
+    
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get(reverse('ordercreate'))
+        self.assertRedirects(response, '/login/?next=/orders/create/')
+    
+    def test_logged_in_uses_correct_template(self):
+        login = self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+        response = self.client.get(reverse('ordercreate'))
+
+        # Check user is logged in
+        self.assertEqual(str(response.context['user']), 'testuser1')
+        # Check that logged in user response "success"
+        self.assertEqual(response.status_code, 200)
+        # Check correct template used
+        self.assertTemplateUsed(response, 'inventory/orderCreateForm.html')
+
+    def test_tz_make_aware(self):
+        tz_name = 'US/Pacific'
+        self.assertEqual(
+            timezone.make_aware(datetime.datetime.now(), timezone=pytz.timezone(tz_name)),
+            datetime.datetime.now(tz=pytz.timezone(tz_name))
+       )
+
+    def test_POST_adds_new_order(self):
+        self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+        tz_name = 'US/Pacific'
+        aware_date = timezone.make_aware(datetime.datetime.now(), timezone=pytz.timezone(tz_name))
+
+        self.client.session['django_timezone'] = tz_name
+        self.client.session.save()
+        
+        response = self.client.post(self.order_create_url, {
+            'item': self.menu_item_1,
+            'time': aware_date
+        })
+        self.assertEquals(response.status_code, 302)
+        print(response.context)
+        
